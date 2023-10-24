dc062b506cb74e36366b65347b984119cd4e00e7
Maxo32 <maxicapo32@outlook.es>
2023-10-24 16:41:08 -0300

    no

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -228,7 +228,7 @@ function esVocal(letra) {
   if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
     return "Es vocal";
   } else {
-    return "Dato incorrecto";
+    return "Dato incorrect";
   }
 }
 
